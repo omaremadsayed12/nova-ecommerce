75521b986a3f737ecd755eaf71ee82e16b780f85
Omar Emad Sayed <mizomar27@gmail.com>
2026-08-19 18:29:28 +0300

    chore: initialize project

---
+++ server/src/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+const app = express();
+
+app.use(helmet());
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "API is running"
+  });
+});
+
+export default app;
